7dd9db5ecf55abca5dcc36a56fb19f4c61a3fbc1
Londiwe27 <londiweprecious@ymail.com>
2022-06-02 13:29:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,2 @@
 #!/bin/bash
- char str [] = "Programming is like building a multilingual puzzle";
-    puts(str);
-    return 0;
+ char str [] = "Programming is like building a multilingual puzzle";puts(str);return 0;
