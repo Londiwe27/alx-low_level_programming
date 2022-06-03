f97cc151a6438773709f928c8749929570f11648
Londiwe27 <londiweprecious@ymail.com>
2022-06-02 18:40:24 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main - Prints the size of various types based on
 * *        the computer it is compiled and run on..
@@ -7,8 +7,8 @@
 */
 int main(void)
 
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		59);
-
 	return (1);
 }
