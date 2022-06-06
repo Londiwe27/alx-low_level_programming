01828bdac8d5d98fcb8a8645a23e6a9b54ac7654
Londiwe27 <londiweprecious@ymail.com>
2022-06-06 13:47:13 -0700

    task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int number1, number2;
+	int num1, num2;
 
-	for (number1 = 0; number1 <= 98; number1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (number2 = number1 + 1; number2 <= 99; number2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar((number1 / 10) + '0');
-			putchar((number1 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar((number2 / 10) + '0');
-			putchar((number2 % 10) + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (number1 == 98 && number2 == 99)
+			if (num1 == 98 && num2 == 99)
 				continue;
 
 			putchar(',');
@@ -31,3 +31,4 @@ int main(void)
 
 	return (0);
 }
+
