393a1475cf21071e576a5933dc21bfdb7724126f
Londiwe27 <londiweprecious@ymail.com>
2022-06-07 11:29:40 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,2 @@
 #include <stdio.h>
-#include "main.h"
-
-/**
- *main -  Printing a  word  follwed  by  a new  line.
-
- * Return: Always 0.
- */
-int main(void)
-{
-	char s[] = "_putchar.\n";
-	print_until_newline(s);
-	puts("\n");
-
-
-	return (0);
-}
-void print_until_newline(char *s)
-{
-	int i = 0;
-	while (s[i] != '/n' &&  s[i] != '/0')
-	{
-		putchar (s[i];
-		i++;
-	}
-
-}
+#include "main."
