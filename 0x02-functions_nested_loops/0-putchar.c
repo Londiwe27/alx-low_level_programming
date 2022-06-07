55bd5b37a3694d3e579dd3a3905ccc1ae522c20c
Londiwe27 <londiweprecious@ymail.com>
2022-06-07 11:56:02 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-#include "main."
+
+/**
+ * _putchar -writes the  chatacter c to stdout
+ * @c: The chacter to print
+ *
+ * Return: Always 0
+ * /
+ into _putch(char c)
+ {
+ 	return (write(1, &c, 1));
+}
