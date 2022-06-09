65992342402034d2272b0fb6cc07ad534686be51
Londiwe27 <londiweprecious@ymail.com>
2022-06-09 16:56:37 -0700

    task 3

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -21,9 +21,6 @@ int convert_day(int month, int day)
 		case 4:
 			day = 90 + day;
 			break;
-		case 5:
-			day = 120 + day;
-			break;
 		case 6:
 			day = 151 + day;
 			break;
@@ -43,7 +40,7 @@ int convert_day(int month, int day)
 			day = 304 + day;
 			break;
 		case 12:
-			day = 334 + day;
+			day = 334 + day
 			break;
 		default:
 			break;
