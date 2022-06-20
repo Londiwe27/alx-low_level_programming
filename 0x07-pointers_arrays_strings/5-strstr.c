f51222eb1d3a5c302c6fd25c4ff5eb8471d6460c
Londiwe27 <londiweprecious@ymail.com>
2022-06-20 12:45:39 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,43 +4,42 @@
  * _strstr - Entry point
  * @haystack: input
  * @needle: input
- * Return: Always 0 (Success)
- */
+ * Return: Always 0
+*/
 
 char *_strstr(char *haystack, char *needle)
-  
 {
-  
-  for (; *haystack != '\0'; haystack++)
-    
-    {
-      
-      char *l = haystack;
-      
-      char *p = needle;
-      
 
-      
-      while (*l == *p && *p != '\0')
-	
+	for (; *haystack != '\0'; haystack++)
+
 	{
-	  
-	  l++;
-	  
-	  p++;
-	  
+
+		char *l = haystack;
+
+		char *p = needle;
+
+
+
+		while (*l == *p && *p != '\0')
+
+		{
+
+			l++;
+
+			p++;
+
+		}
+
+
+
+		if (*p == '\0')
+
+			return (haystack);
+
 	}
-      
 
-      
-      if (*p == '\0')
-	
-	return (haystack);
-      
-    }
-  
 
-  
-  return (0);
-  
+
+	return (0);
+
 }
