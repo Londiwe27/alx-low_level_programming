cac073f9a1b96dc1c68f8d52e6e56e040eee1074
Londiwe27 <londiweprecious@ymail.com>
2022-06-27 11:51:25 -0700

    task 3 and 4

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,8 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 int main(int argc, char __attribute__((__unused__)) *argv[])
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
+int arg;
 int main(int argc, char *argv[])
 	int num1, num2, prod;
+	int main(int argc, char *argv[])
+	int num, digit, sum = 0;
