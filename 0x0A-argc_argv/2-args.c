b1b65562e596d048750b53074802257d59afa912
Londiwe27 <londiweprecious@ymail.com>
2022-06-28 06:02:59 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include<stdio.h>
 
 /**
  * main - Prints all arguments it receives.
@@ -10,10 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	int arg;
+	int i;
 
-	for (arg = 0; arg < argc; arg++)
-		printf("%s\n", argv[arg]);
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
 
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,9 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 int main(int argc, char __attribute__((__unused__)) *argv[])
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 int arg;
 int main(int argc, char *argv[])
-	int num1, num2, prod;
-	int main(int argc, char *argv[])
-	int num, digit, sum = 0;
+int num1, num2, prod;
+int main(int argc, char *argv[])
+int num, digit, sum = 0;
+int _putchar(char);
