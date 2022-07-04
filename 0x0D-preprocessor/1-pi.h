6715061a2b8bf43fc24c331507388a3068793954
Londiwe27 <londiweprecious@ymail.com>
2022-07-04 11:54:28 -0700

    task 1 and 2

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI-H
+#define PI_H
+
+/*
+ * File:1-pi.h
+ * Desc: Header file that defines a macro named PI
+ *       SIZE as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,3 @@
+int main(void)
+	int _putchar(char C);
+
